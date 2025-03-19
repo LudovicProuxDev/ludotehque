59d73f8f44b5eeeb5e9b3fb79511a3fd7f97becb
LudovicProuxDev <ludovic.proux@outlook.fr>
2025-03-19 16:21:12 +0100

    Creation projet

---
+++ src/test/java/com/example/ludotehque/LudotehqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ludotehque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudotehqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
