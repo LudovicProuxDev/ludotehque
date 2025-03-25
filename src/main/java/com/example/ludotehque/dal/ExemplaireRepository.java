7af81ed61ceddc7245b0e1d7597a9773f8ead0ed
LudovicProuxDev <ludovic.proux@outlook.fr>
2025-03-25 10:25:25 +0100

    update repositories with new methods

--- src/main/java/com/example/ludotehque/dal/ExemplaireRepository.java
+++ src/main/java/com/example/ludotehque/dal/ExemplaireRepository.java
@@ -2,6 +2,22 @@ package com.example.ludotehque.dal;
 
 import com.example.ludotehque.bo.Exemplaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ExemplaireRepository extends JpaRepository<Exemplaire, Integer> {
+    @Query(nativeQuery = true,
+            value="select count(e.codebarre) from jeu j inner join exemplaire e "
+                    + "on j.no_jeu = e.no_jeu "
+                    + "where j.no_jeu = :noJeu "
+                    + "and e.louable = 1 "
+                    + "and e.no_exemplaire not in "
+                    + "(select l.no_exemplaire from locations l "
+                    + "where l.date_retour IS null "
+                    + "and l.no_exemplaire = e.no_exemplaire) "
+                    + " group by e.code_barre ")
+    int nbExemplairesDisponibleByNoJeu(@Param("noJeu") Integer noJeu);
+
+    @Query("SELECT e FROM Exemplaire e LEFT JOIN FETCH e.jeu WHERE e.codeBarre = :codebarre")
+    Exemplaire findByCodebarreWithJeu(@Param("codebarre") String codebarre);
 }
--- src/main/java/com/example/ludotehque/dal/LocationRepository.java
+++ src/main/java/com/example/ludotehque/dal/LocationRepository.java
@@ -2,6 +2,10 @@ package com.example.ludotehque.dal;
 
 import com.example.ludotehque.bo.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+    @Query("SELECT l FROM Location l WHERE l.exemplaire.codeBarre = :codebarre")
+    Location findLocationByCodebarreWithJeu(@Param("codebarre") String codebarre);
 }
